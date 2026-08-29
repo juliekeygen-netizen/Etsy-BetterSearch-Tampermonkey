9f7a5ddbc57d4b650ee35a0303bcbeab63ea023b
juliekeygen-netizen <julie.keygen@gmail.com>
2026-08-29 18:02:46 +0300

    chore: remove obsolete AutoPull reset launcher

--- BetterSearch_AutoPull_Notify/Reset setup.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-setlocal
-cd /d "%~dp0"
-if exist "%~dp0BetterSearch-AutoPull.config.json" del /q "%~dp0BetterSearch-AutoPull.config.json"
-echo Saved Repo AutoPull setup removed.
-echo.
-powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -File "%~dp0BetterSearch-AutoPull.ps1" -Setup
